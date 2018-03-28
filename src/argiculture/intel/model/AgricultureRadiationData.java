f56f74c067f4032a180cffba9099ce2bbe19e8b5
HJS <407410014@qq.com>
2018-03-28 16:07:28 +0800

    Intital commit with Models

---
+++ src/argiculture/intel/model/AgricultureRadiationData.java
@@ -0,0 +1,104 @@
+package argiculture.intel.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * 农业数据辐射表
+ *
+ *
+ * @author Stephen Huge
+ *
+ * @date 2018年3月28日下午3:52:35
+ *
+ */
+@Entity
+@Table(name="agri_data_ radiation")
+public class AgricultureRadiationData {
+	
+	private int radiationDataId;
+	
+	private Date tacq;
+	
+	private String batteryLevel;
+	
+	private String currentSolarRadiationIntensity;
+	
+	private String cumulativeSolarRadiationIntensity;
+	
+	private String imei;
+	
+	private String meteorologicalEquipmentStatus;
+	
+	private Date serverTime;
+
+	@Id
+	public int getRadiationDataId() {
+		return radiationDataId;
+	}
+
+	public void setRadiationDataId(int radiationDataId) {
+		this.radiationDataId = radiationDataId;
+	}
+
+	public Date getTacq() {
+		return tacq;
+	}
+
+	public void setTacq(Date tacq) {
+		this.tacq = tacq;
+	}
+
+	public String getBatteryLevel() {
+		return batteryLevel;
+	}
+
+	public void setBatteryLevel(String batteryLevel) {
+		this.batteryLevel = batteryLevel;
+	}
+
+	public String getCurrentSolarRadiationIntensity() {
+		return currentSolarRadiationIntensity;
+	}
+
+	public void setCurrentSolarRadiationIntensity(String currentSolarRadiationIntensity) {
+		this.currentSolarRadiationIntensity = currentSolarRadiationIntensity;
+	}
+
+	public String getCumulativeSolarRadiationIntensity() {
+		return cumulativeSolarRadiationIntensity;
+	}
+
+	public void setCumulativeSolarRadiationIntensity(String cumulativeSolarRadiationIntensity) {
+		this.cumulativeSolarRadiationIntensity = cumulativeSolarRadiationIntensity;
+	}
+
+	public String getImei() {
+		return imei;
+	}
+
+	public void setImei(String imei) {
+		this.imei = imei;
+	}
+
+	public String getMeteorologicalEquipmentStatus() {
+		return meteorologicalEquipmentStatus;
+	}
+
+	public void setMeteorologicalEquipmentStatus(String meteorologicalEquipmentStatus) {
+		this.meteorologicalEquipmentStatus = meteorologicalEquipmentStatus;
+	}
+
+	public Date getServerTime() {
+		return serverTime;
+	}
+
+	public void setServerTime(Date serverTime) {
+		this.serverTime = serverTime;
+	}
+	
+	
+}
---
+++ src/argiculture/intel/model/DeviceInfo.java
@@ -0,0 +1,74 @@
+package argiculture.intel.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+/**
+ * device_info表格映射model
+ *
+ * @author Stephen Huge
+ *
+ * @date 2018年3月28日下午3:32:21
+ *
+ */
+
+@Entity
+@Table(name="device_inf")
+public class DeviceInfo {
+	
+	private int deviceId;
+	private String deviceName;
+	private String imei;
+	private String unitType;
+	private String address;
+	private Date date;
+	
+	// 默认无参构造方法
+	public DeviceInfo() {}
+	/*
+	 * 设备的ID
+	 */
+	@Id
+	public int getDeviceId() {
+		return deviceId;
+	}
+
+	public void setDeviceId(int deviceId) {
+		this.deviceId = deviceId;
+	}
+
+	public String getDeviceName() {
+		return deviceName;
+	}
+	public void setDeviceName(String deviceName) {
+		this.deviceName = deviceName;
+	}
+	public String getImei() {
+		return imei;
+	}
+	public void setImei(String imei) {
+		this.imei = imei;
+	}
+	public String getUnitType() {
+		return unitType;
+	}
+	public void setUnitType(String unitType) {
+		this.unitType = unitType;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	
+	
+}
