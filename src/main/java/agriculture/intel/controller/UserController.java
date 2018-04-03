bc93567990d7d02231f39049b7279600356243d4
HJS <407410014@qq.com>
2018-04-03 17:09:38 +0800

    add mapping of index.jsp and some other change

--- src/main/java/agriculture/intel/controller/UserController.java
+++ src/main/java/agriculture/intel/controller/UserController.java
@@ -1,33 +1,45 @@
 package main.java.agriculture.intel.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-import com.sun.corba.se.impl.protocol.giopmsgheaders.RequestMessage;
 
 import main.java.agriculture.intel.model.AdministratorInfo;
 import main.java.agriculture.intel.service.AdministratorInfoService;
 
 @Controller
-@RequestMapping("/")
 public class UserController {
 	
 	private AdministratorInfoService administratorInfoService; 
 	
-	@RequestMapping(value="addAdministrator", method=RequestMethod.GET)
-	public String addAdministrator() {
+	/**
+	 * 访问项目名默认直接跳转到index.jsp 
+	 */
+	@RequestMapping(value = "/", method = RequestMethod.GET)
+	public String welcomePage(Model model) {
+	    return "index";
+	}
+	@RequestMapping(value="/addAdministrator", method=RequestMethod.GET)
+	public String addAdministrator(Model model) {
+		model.addAttribute("adminInfo", new AdministratorInfo());
 		return "addAdmin";
 	}
 	@RequestMapping(value="/addAdministrator", method=RequestMethod.POST)
-	public String addAdministrator(AdministratorInfo administratorInfo) {
-		administratorInfoService.addAdministratorInfo(administratorInfo);
+	public String addAdministrator(AdministratorInfo adminInfo, Model model) {
+		model.addAttribute("adminInfo", adminInfo);
+		administratorInfoService.addAdministratorInfo(adminInfo);
 		return "allAdministrators";
 	}
 	
-	@RequestMapping("allAdministrators")
-	public String getAllAdministrators() {
-		administratorInfoService.getAllAdministratorInfo();
+	@RequestMapping("/allAdministrators")
+	public String getAllAdministrators(ModelMap modelMap) {
+		List<AdministratorInfo> adminList = administratorInfoService.getAllAdministratorInfo();
+		modelMap.addAttribute(adminList);
 		return "administrators";
 	}
 	
